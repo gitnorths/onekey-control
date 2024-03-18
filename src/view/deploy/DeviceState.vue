c8ed6a5a2dd21e4d4d65e9ef50244b502a998705
黄欢 <huanghuan.207791@sieyuan.com>
2024-03-18 11:14:16 +0800

    feat: 增加github自动化部署

--- src/view/deploy/DeviceState.vue
+++ src/view/deploy/DeviceState.vue
@@ -25,38 +25,47 @@
       </el-form>
     </div>
     <div class="oc-box__main">
-      <div class="device-state__text">
+      <!-- <div class="device-state__text">
         <dv-decoration-11 style="width: 500px; height: 60px">
           <el-text size="large">{{ stationName }}-设备态解析结果</el-text>
         </dv-decoration-11>
-      </div>
+      </div> -->
       <el-table
         :data="tableData"
         :span-method="objectSpanMethod"
         style="width: 100%"
       >
-        <el-table-column
-          v-for="(item, i) in tableColumn"
-          :prop="item.prop"
-          :label="item.label"
-          :width="item.width ? item.width : null"
-          :key="i"
-        >
-          <template #default="scope">
-            <el-link
-              @click="handleEdit(scope.$index, scope.row)"
-              type="success"
-            >
-              修正
-            </el-link>
-            <el-link
-              @click="handleEdit(scope.$index, scope.row)"
-              type="success"
-            >
-              确认
-            </el-link>
-          </template>
-        </el-table-column>
+        <template v-for="(item, i) in tableColumn" :key="i">
+          <el-table-column
+            v-if="item.prop != 'operator'"
+            :prop="item.prop"
+            :label="item.label"
+            :width="item.width ? item.width : null"
+          />
+          <el-table-column
+            :prop="item.prop"
+            :label="item.label"
+            :width="item.width ? item.width : null"
+            v-else
+          >
+            <template #default="scope">
+              <el-button
+                type="warning"
+                size="small"
+                @click="handleEdit(scope.$index, scope.row)"
+              >
+                修正
+              </el-button>
+              <el-button
+                type="success"
+                size="small"
+                @click="handleEdit(scope.$index, scope.row)"
+              >
+                确认
+              </el-button>
+            </template>
+          </el-table-column>
+        </template>
       </el-table>
     </div>
     <el-dialog
@@ -100,7 +109,7 @@
   </div>
 </template>
 <script setup>
-import { computed, onMounted, reactive, ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import { ElMessage, ElMessageBox } from "element-plus";
 import { mokeGet } from "@/api";
 
@@ -109,10 +118,11 @@ const stationName = ref(null);
 const stationOptions = ref([]);
 
 const tableColumn = ref([
-  { prop: "index", label: "序号", width: "70px" },
+  { prop: "index", label: "序号", width: "100px" },
   { prop: "state", label: "状态" },
   { prop: "condition", label: "条件" },
   { prop: "parseResults", label: "解析结果" },
+  { prop: "operator", label: "操作", width: "150px" },
 ]);
 const tableData = ref([]);
 
@@ -123,17 +133,6 @@ const uploadForm = reactive({});
 const uploadRef = ref();
 const uploadUrl = ref("/api/uploadOptabInfo");
 const uploadFileList = ref([]);
-
-// 获取所有单元格合并数据
-const spanArr = computed(() => {
-  if (!tableData.value.length) return [];
-  const mergeCols = ["index"]; // 需要合并的列（字段）
-  return getMergeCells(tableData.value, tableColumn.value, mergeCols);
-});
-
-const objectSpanMethod = ({ row, column, rowIndex, columnIndex }) => {
-  return spanArr[rowIndex][columnIndex];
-};
 
 //查询场站
 const getStation = () => {
@@ -177,7 +176,10 @@ const getDevstatus = (value) => {
         });
       }
     }
-    console.log("tableData", tableData.value);
+
+    // 给列表赋值
+    const colFields = ["index", "state"];
+    setTableRowSpan(tableData.value, colFields);
     // tableData.value = [
     //   {
     //     index: "1",
@@ -252,6 +254,43 @@ const submitUpload = () => {
 onMounted(() => {
   getStation();
 });
+
+// 设置合并的行和列
+const setTableRowSpan = (tableData, colFields) => {
+  let lastItem = [];
+  // 循环需要合并的列
+  colFields.forEach((field, index) => {
+    tableData.forEach((item) => {
+      // 存值，把合并字段存入行，为了合并单元格时检索列是否含有该字段
+      item.mergeCell = colFields;
+      // 合并字段出现的次数
+      const rowSpan = `rowspan_${field}`;
+      // 比较上一次的存值和该轮的合并字段，判断是否合并到上个单元格
+      if (colFields.slice(0, index + 1).every((e) => lastItem[e] === item[e])) {
+        // 如果是，合并行
+        item[rowSpan] = 0; // 该轮合并字段数量存0
+        // 上轮合并字段数量+1
+        lastItem[rowSpan] += 1;
+      } else {
+        // 初始化进入&& 如果不是，完成一次同类合并，lastItem重新赋值，进入下一次计算
+        item[rowSpan] = 1; // 该轮合并字段第一次出现，数量存1
+        // 改变比较对象，重新赋值，进行下一次计算
+        lastItem = item;
+      }
+    });
+  });
+};
+
+const objectSpanMethod = ({ row, column, rowIndex, columnIndex }) => {
+  if (row.mergeCell.includes(column.property)) {
+    const rowspan = row[`rowspan_${column.property}`];
+    if (rowspan) {
+      return { rowspan: rowspan, colspan: 1 };
+    } else {
+      return { rowspan: 0, colspan: 0 };
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
